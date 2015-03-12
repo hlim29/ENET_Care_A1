67decccd30b693c3adcf13d0fc06c55ac3d75dd2
AlbertSamuel <albertsamuelhussen@gmail.com>
2015-03-12 21:55:30 +1100

    Innitial Commit

---
+++ ENET Care System/ENET_Care_Console/Agent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENET_Care_Console
+{
+    class Agent
+    {
+        private bool Login() { return true; }
+    }
+}
---
+++ ENET Care System/ENET_Care_UnitTest/AgentTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ENET_Care_UnitTest
+{
+    [TestClass]
+    public class AgentTest
+    {
+        //[TestMethod]
+        //public void Login_AgentPassword_True()
+        //{
+        //    Assert.AreEqual(true, Agent.Login("Agent", "Password"));
+        //}
+        //[TestMethod]
+        //public void Register_GenerateBarcode_Numbers()
+        //{
+        //    Assert.AreEqual(12344223773, Agent.GetBarcode());
+        //}
+        //[TestMethod]
+        //public void Register_InsertPolioVacination_PolioVacination()
+        //{
+        //    Agent.SelectType("Polio Vaccination");
+        //    Assert.AreEqual("Polio Vacination", Agent.Type);
+        //}
+        //[TestMethod]
+        //public void Register_InsertExpiryDate_ExpiryDate()
+        //{
+        //    Agent.InsertExpiryDate("11/11/17");
+        //    Assert.AreEqual("11/11/17", Agent.ExpiryDate);
+        //}
+    }
+}
