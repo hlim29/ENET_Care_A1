e5e5a7c90fabcb779fb431240c72d3ed1005ce30
AndreaSa11866694 <11866694@student.uts.edu.au>
2015-03-12 21:29:30 +1100

    UnitTestPackage

---
+++ ENET Care System/ENET_Care_Console/Package.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENET_Care_Console
+{
+    public class Package
+    {
+        public int barCode;
+        public int medicationId;
+        public DateTime ExpiryDate;
+        public int quantity;
+        public void AddPackage()
+        {
+
+        }
+    }
+}
---
+++ ENET Care System/ENET_Care_UnitTest/PackageTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENET_Care_Console;
+
+namespace ENET_Care_UnitTest
+{
+    [TestClass]
+    public class PackageTest
+    {
+        Package _package;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _package = new Package();
+        }
+
+        [TestMethod]
+        public void RegisterPackage_CkeckFieldBarCode_FieldBarCodeFilled()
+        {
+            Assert.IsNotNull(_package.barCode);
+        }
+
+        [TestMethod]
+        public void RegisterPackage_CkeckFieldExpiryDate_FieldExpiryDateFilled()
+        {
+            Assert.IsNotNull(_package.ExpiryDate);
+        }
+    }
+}
