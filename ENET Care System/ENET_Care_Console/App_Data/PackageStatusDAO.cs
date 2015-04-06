d81ed6ac7dc5bda051382e64b568900e8e908fd9
AndreaSa11866694 <11866694@student.uts.edu.au>
2015-04-07 01:57:26 +1000

    Insert DAO classes

---
+++ ENET Care System/ENET_Care_Console/App_Data/PackageStatusDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENET_Care.Data.App_Data
+{
+    class PackageStatusDAO
+    {
+    }
+}
