81e001840bd32a40757ab398467459efa20ea7ce
AlbertSamuel <albertsamuelhussen@gmail.com>
2015-04-06 18:29:58 +1000

    Finished Agent Test Scenario.

--- ENET Care System/ENET_Care_UnitTest/AgentTest.cs
+++ ENET Care System/ENET_Care_UnitTest/AgentTest.cs
@@ -18,41 +18,115 @@ namespace ENET_Care_UnitTest
 
         /**
          * Registering new package and assigning to current Distribution Centre
-         * Checkin if the package got the barcode or not.
-         * Assuming Agent.RegisterPackage return a package object.
+         * Checking if the package got the barcode or not.
+         * Checking if the PackageStatus for the new package has been created.
+         * 
+         * ASSUMPTION: Agent.RegisterPackage return a package object.
          * */
         [TestMethod]
-        public void RegisterPackage_PackageDetails_PackageAdded()
+        public void RegisterPackage_PackageDetails_BarcodeExist()
         {
-            //Package newPackage = _agent.RegisterPackage("100 Medication Pills", "12/12/17");
+            //NOTE: Uncomment when method already updated
+
+            //Package newPackage = _agent.RegisterPackage(createMockMedication(), new DateTime(17,12,12));
             //Assert.AreNotEqual(null, newPackage.BarCode);
+            //Assert.AreNotEquals(null, newPackage.getPackageStatus());
         }
+
+        /**
+         * Send Package method
+         * Creating Mock Object for Package and Package Status
+         * Updating Package Status 'Status' and 'Centre Destination' property
+         * */
         [TestMethod]
         public void SendPackage_PackageBarcode_PackageStatusChanged()
         {
-            int barcode = 123456;
-            var dummyPackage = new Moq.Mock<Package>();
-            var dummyPackageStatus = new Moq.Mock<PackageStatus>();
-            dummyPackage.SetupProperty(package => package.BarCode, barcode);
-            Package packageObject = dummyPackage.Object;
-            dummyPackageStatus.SetupProperty(packageStatus => packageStatus.Package, packageObject);            
-            PackageStatus packageStatusObject = dummyPackageStatus.Object;
-            _agent.SendPackage(barcode);
-            Assert.Equals("onTransit", packageStatusObject.Status);
+            //NOTE: Uncomment when method ready to be tested
+
+            DistributionCentre destination = new Moq.Mock<DistributionCentre>().Object;
+            Package packageObject = createMockPackage();
+            //PackageStatus packageStatusObject = packageObject.getPackageStatus();
+            //_agent.SendPackage(barcode, destination); //Uncomment when method already updated
+            //Assert.Equals("onTransit", packageStatusObject.Status);
+            //Assert.Equals(destination, packageStatusObject.CentreDestination);
         }
 
+        /**
+         * Receiving package
+         * Change the Package Status when received to "InStock"
+         * Confirming that centre source is changed
+         * ASSUMPTION : Current package location written in packageSource
+         * */
         public void ReceivePackage_PackageBarcode_PackageStatusChanged()
         {
-            int barcode = 123456;
+            //NOTE: Uncomment when method ready to be tested
+
+            Package packageObject = createMockPackage();
+            //PackageStatus packageStatusObject = packageObject.getPackageStatus();
+            _agent.ReceivePackage(packageObject.BarCode);
+            //Assert.Equals("inStock", packageStatusObject.Status);
+            //Assert.Equals(_agent.DistributionCentre, packageStatusObject.CentreSource);
+        }
+        /**
+         * Discarding Package
+         * Change the Status property in PackageStatus becomes True
+         * ASSUMPTION: Removing and Discarding only change the status become "Inactive" (Status becomes isDeleted)
+         *             Do not really delete in the database.
+         * */
+        [TestMethod]
+        public void DiscardPackage_PackageBarcode_PackageNotShown()
+        {
+            //NOTE: Uncomment when code ready to be tested
+
+            Package dummyPackage = createMockPackage();
+            //_agent.RegisterPackage(createMockMedication(), dummyPackage.ExpiryDate);
+            //_agent.DiscardPackage(dummyPackage.BarCode);
+            //PackageStatus status = dummyPackage.getPackageStatus();
+            //Assert.Equals("discarded", status.Status);
+        }
+
+        /**
+         * Removing lost package
+         * Need a collection of scanned barcode than delete the package that is not scanned
+         * 
+         * Explanation:
+         *      Get all barcodes existed in the database
+         *      Register a new package (the new package barcode will not be in the collection)
+         *      Call RemoveLostPackageMethod
+         *      The new package should be marked as lost.
+         *      
+         * NOTE: Audit only happen in its own Medical Centre, will not change package from other Medical Centre
+         * */
+        public void RemoveLostPackage_BarcodeCollection_StatusChanged()
+        {
+            //NOTE: Uncomment when method ready to be tested
+
+            //retrieving all barcodes exist in the database
+            int[] barcodes = new int[5] { 1111111, 22222222, 33333333, 44444444, 5555555 }; //updated later
+            Package dummyPackage = createMockPackage();
+            //_agent.RegisterPackage(createMockMedication(), dummyPackage.ExpiryDate);
+            //_agent.RemoveLostPackage(barcodes);
+            //Assert.Equals("lost", dummyPackage.getPackageStatus().Status);
+        }
+        /**
+         * Addition method to create a mock object
+         * */
+        private Package createMockPackage()
+        {
             var dummyPackage = new Moq.Mock<Package>();
-            var dummyPackageStatus = new Moq.Mock<PackageStatus>();
-            dummyPackage.SetupProperty(package => package.BarCode, barcode);
-            Package packageObject = dummyPackage.Object;
-            dummyPackageStatus.SetupProperty(packageStatus => packageStatus.Package, packageObject);            
-            PackageStatus packageStatusObject = dummyPackageStatus.Object;
-            _agent.ReceivePackage(123456);
-            Assert.Equals("inStock", packageStatusObject.Status);
-            Assert.Equals(_agent.DistributionCentre, packageStatusObject.CentreSource);
+            dummyPackage.SetupProperty(package => package.BarCode, 123456)
+                .SetupProperty(package => package.ExpiryDate, new DateTime(17, 12, 12));
+            return dummyPackage.Object;
+        }
+        private MedicationStandardType createMockMedication()
+        {
+            var dummyMedication = new Moq.Mock<MedicationStandardType>();
+            dummyMedication.SetupProperty(medication => medication.Quantity, 100)
+                .SetupProperty(medication => medication.Description, "Medication Pills")
+                .SetupProperty(medication => medication.Cost, 30.00)
+                .SetupProperty(medication => medication.IsTempSensitive, false)
+                .SetupProperty(medication => medication.ShelfLife, "12/12/17");
+            return dummyMedication.Object;
         }
     }
 }
--- ENET Care System/ENET_Care_UnitTest/PackageTest.cs
+++ ENET Care System/ENET_Care_UnitTest/PackageTest.cs
@@ -9,12 +9,48 @@ namespace ENET_Care_UnitTest
     {
         Package _package;
 
+        /**
+         * ASSUMPTION: Constructor Package(Medication, ExpiryDate)
+         * */
         [TestInitialize]
         public void Setup()
         {
-            _package = new Package();
+            //NOTE: Uncomment when method ready to be tested
+
+            //Package _package = new Package(createMockMedication(), new DateTime(17, 12, 12));
+        }
+        /**
+         * A package should always have a package status.
+         * Checking if the PackageStatus has been created or not in PackageRegistration.
+         * NOTE: getPackageStatus() should search its own packageID in the collections
+         * */
+        [TestMethod]
+        public void getPackageStatus_packageObject_Status()
+        {
+            //NOTE: Uncomment when method ready to be tested.
+
+            //PackageStatus status = _package.getPackageStatus();
+            //Assert.Equals("inStock", status.Status);
         }
 
+        /**
+         * Addition method to create a mock object
+         * */
+        private MedicationStandardType createMockMedication()
+        {
+            var dummyMedication = new Moq.Mock<MedicationStandardType>();
+            dummyMedication.SetupProperty(medication => medication.Quantity, 100)
+                .SetupProperty(medication => medication.Description, "Medication Pills")
+                .SetupProperty(medication => medication.Cost, 30.00)
+                .SetupProperty(medication => medication.IsTempSensitive, false)
+                .SetupProperty(medication => medication.ShelfLife, "12/12/17");
+            return dummyMedication.Object;
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /**
+         * Here below is a dummy testing, will be finalised soon
+         * */
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
         [TestMethod]
         public void RegisterPackage_CheckFieldBarCode_FieldBarCodeFilled()
         {
