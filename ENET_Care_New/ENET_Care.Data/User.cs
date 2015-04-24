f8091cbeb5839b3e357848b734410a059390a353
AndreaSa11866694 <11866694@student.uts.edu.au>
2015-04-25 01:18:40 +1000

    Deleting not used classes

--- ENET_Care_New/ENET_Care.Data/User.cs
+++ ENET_Care_New/ENET_Care.Data/User.cs
@@ -10,6 +10,11 @@ namespace ENET_Care.Data
 {
     public class User
     {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public DistributionCentre DistributionCentre { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
