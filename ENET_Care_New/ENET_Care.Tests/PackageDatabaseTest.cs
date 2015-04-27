c6cab0668885cb6bd2b1c17d6231f6f16ef9f891
AlbertSamuel <albertsamuelhussen@gmail.com>
2015-04-27 16:03:38 +1000

    PackageDatabaseTest

---
+++ ENET_Care_New/ENET_Care.Tests/PackageDatabaseTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data;
+using ENET_Care.Data;
+using ENET_Care.Business;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ENET_Care.Data.DataSetTableAdapters;
+
+
+namespace ENET_Care.Tests
+{
+    [TestClass]
+    public class PackageDatbaseTest
+    {
+        [AssemblyInitialize]
+        public static void SetupDataDirectory(TestContext context)
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path.GetFullPath(@"..\..\..\ENET_Care_New\App_Data"));
+        }
+
+        [TestMethod]
+        public void PackageDatabaseTest_InsertTable_TableAdded()
+        {
+            Package package = new Package();
+            package.BarCode = 123455;
+            package.Medication = PackageLogic.GetMedicationList().Last.Value;
+            package.ExpiryDate = new DateTime(2017, 10, 10);
+            package.Quantity = 100;
+
+            PackageDAO packageDAO = new PackageDAO();
+            packageDAO.insertPackage(package);
+
+            int lastId = new PackageTableAdapter().GetData().Count;
+            Assert.AreEqual(package.ExpiryDate, new PackageTableAdapter().GetData().FindByPackageId(lastId).ExpiryDate, "Expiry Date are not the same!");
+            Assert.AreEqual(package.Quantity, new PackageTableAdapter().GetData().FindByPackageId(lastId).Quantity, "Quantity are not the same!");
+        }
+
+    }
+}
