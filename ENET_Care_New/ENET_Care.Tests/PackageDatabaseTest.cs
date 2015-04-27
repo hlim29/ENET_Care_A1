4a07607aa74446d5d1173a0247764bfa4035004c
AlbertSamuel <albertsamuelhussen@gmail.com>
2015-04-27 18:24:56 +1000

    Package Database Test GetPackage&Register

--- ENET_Care_New/ENET_Care.Tests/PackageDatabaseTest.cs
+++ ENET_Care_New/ENET_Care.Tests/PackageDatabaseTest.cs
@@ -38,6 +38,35 @@ namespace ENET_Care.Tests
             Assert.AreEqual(package.ExpiryDate, new PackageTableAdapter().GetData().FindByPackageId(lastId).ExpiryDate, "Expiry Date are not the same!");
             Assert.AreEqual(package.Quantity, new PackageTableAdapter().GetData().FindByPackageId(lastId).Quantity, "Quantity are not the same!");
         }
+        [TestMethod]
+        public void PackageDatabaseTest_GetPackageByBarcode_CorrectPackageAdded()
+        {
+            Package package = new Package();
+            package.BarCode = 123456;
+            package.Medication = PackageLogic.GetMedicationList().Last.Value;
+            package.ExpiryDate = new DateTime(2017, 10, 10);
+            package.Quantity = 100;
+
+            PackageDAO packageDAO = new PackageDAO();
+            packageDAO.insertPackage(package);
+
+            Assert.AreEqual(package.ExpiryDate, new Package().GetPackageByBarcode(package.BarCode).BarCode);
+        }
+        [TestMethod]
+        public void PackageDatabaseTest_RegisterPackage_PackageAdded()
+        {
+            Package package = new Package();
+            package.BarCode = 123457;
+            package.Medication = PackageLogic.GetMedicationList().Last.Value;
+            package.ExpiryDate = new DateTime(2017, 10, 10);
+            package.Quantity = 100;
+
+            Package pkg = new Package();
+            pkg.RegisterPackage(package);
+
+            Assert.AreEqual(package.ExpiryDate, new PackageTableAdapter().GetData().FindByPackageId(lastId).ExpiryDate, "Expiry Date are not the same!");
+            Assert.AreEqual(package.Quantity, new PackageTableAdapter().GetData().FindByPackageId(lastId).Quantity, "Quantity are not the same!");
+        }
 
     }
 }
